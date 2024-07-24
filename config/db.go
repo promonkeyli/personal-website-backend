b9a31f722b89cf4b9a6e3819307e1c98f6a9ac38
Yang Li <yang.li@mprobe.com>
2024-07-24 14:26:22 +0800

    feat(db.go): modify config db.go

--- config/db.go
+++ config/db.go
@@ -1,3 +1,3 @@
 package config
 
-const MySql = "root:ly@15984093508@tcp(127.0.0.1:3306)/web?charset=utf8mb4&parseTime=True&loc=Local"
+const MySql = "root:ly@15984093508@tcp(localhost:3306)/web?charset=utf8mb4&parseTime=True&loc=Local"
