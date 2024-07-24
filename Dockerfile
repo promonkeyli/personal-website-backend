7206a30dd5087dad4e475185ee8215950e823350
Yang Li <yang.li@mprobe.com>
2024-07-24 09:49:15 +0800

    ci(deploy): modify docker-compose files

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,11 @@
-# Golang镜像版本(构建阶段)
 FROM golang:1.22.5-alpine AS build
-# 工作目录
 WORKDIR /app
-# 依赖下载
 COPY go.mod go.sum ./
 RUN go mod download
-# 复制项目代码到工作目录
 COPY . .
-# Golang 主应用打包
 RUN go build -o main ./cmd/main.go
-# Golang api 文档打包
-RUN go build -o swagger ./cmd/swagger.go
 
-# 使用官方的更小的镜像（运行阶段）
 FROM alpine:latest
-# 设置工作目录
 WORKDIR /root/
-# 从构建阶段复制编译后的二进制文件
 COPY --from=build /app/main .
-COPY --from=build /app/swagger .
-
-# 启动Go 程序
-CMD ["sh", "-c", "./main & ./swagger"]
+CMD ["sh", "-c", "./main"]
--- internal/router/swagger.go
+++ internal/router/swagger.go
@@ -1,19 +1,12 @@
-package main
+package router
 
 import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	_ "web_backend.com/m/v2/api/swagger"
-	"web_backend.com/m/v2/internal/app/middleware"
 )
 
-func main() {
-	r := gin.Default() // 初始化gin实例
-	r.Use(middleware.CorsMiddleware())
+func GenSwaggerRouters(r *gin.Engine) {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	err := r.Run(":8081")
-	if err != nil {
-		return
-	}
 }
