488fc12f310d81c5ea6af69807270263ddef6fe6
young <liyang_email@163.com>
2024-07-26 23:51:16 +0800

    ci(docker): docker build modify

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"os"
 	"web_backend.com/m/v2/internal/app/repository"
 	"web_backend.com/m/v2/internal/router"
 )
 
-func main() {
-	//mode := os.Getenv("GIN_MODE")
-	//if mode == "" {
-	//	mode = gin.DebugMode
-	//}
-	gin.SetMode(gin.ReleaseMode)
+// main 执行前，执行数据库连接，读取数据库环境变量
+func init() {
+	mode := os.Getenv("GIN_MODE")
+	if mode == "" {
+		mode = gin.DebugMode
+	}
+	gin.SetMode(mode)
 	repository.ConnectDB()
+}
+
+func main() {
 	r := router.Router()
 	err := r.Run(":8081")
 	if err != nil {
--- config/db.go
+++ config/db.go
@@ -13,5 +13,4 @@ var dbName = os.Getenv("DB_NAME")
 
 var Release_MySql = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 
-// const Local_Mysql = "root:ly@15984093508@tcp(127.0.0.1:3306)/?charset=utf8mb4"
-const Local_Mysql = "root:ly@15984093508@tcp(website-app:3306)/?charset=utf8mb4"
+const Local_Mysql = "root:ly@15984093508@tcp(127.0.0.1:3306)/?charset=utf8mb4"
