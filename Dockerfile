0b85dd61c0cb2039e3c7436dfd843949c7403cef
young <liyang_email@163.com>
2024-07-27 21:17:32 +0800

    ci(docker): add wait-for-it.sh scripts

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ COPY go.mod go.sum ./
 RUN go mod download
 COPY . .
 RUN go build -o main ./cmd/main.go
+
 FROM alpine:latest
 WORKDIR /root/
 COPY --from=build /app/main .
 ENV GIN_MODE=release \
-    DB_HOST=website-mysql \
+    DB_HOST=10.0.16.5 \
     DB_PORT=3306 \
     DB_USER=root \
     DB_PASSWORD=ly15984093508 \
--- cmd/main.go
+++ cmd/main.go
@@ -1,61 +1,26 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
-	"gorm.io/gorm"
+	"os"
+	"web_backend.com/m/v2/internal/app/repository"
+	"web_backend.com/m/v2/internal/router"
 )
 
-// main 执行前，执行数据库连接，读取数据库环境变量
 func init() {
-	//mode := os.Getenv("GIN_MODE")
-	//if mode == "" {
-	//	mode = gin.DebugMode
-	//}
-	//gin.SetMode(mode)
-	//repository.ConnectDB()
+	mode := os.Getenv("GIN_MODE")
+	if mode == "" {
+		mode = gin.DebugMode
+	}
+	gin.SetMode(mode)
+	repository.ConnectDB()
 }
 
-var (
-	DB  *gorm.DB
-	err error
-)
-
 func main() {
-	//r := router.Router()
-	//gin.DefaultWriter = os.Stdout
-	//err := r.Run(":8081")
-	//if err != nil {
-	//	return
-	//}
-	//const Local_Mysql = "root:ly15984093508@tcp(124.222.140.95:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
-	//
-	//DB, err = gorm.Open(mysql.Open(Local_Mysql), &gorm.Config{})
-	//if err != nil {
-	//	fmt.Println("数据库连接失败", err)
-	//	return
-	//}
-	//fmt.Println("数据库连接成功")
-
-	// 连接数据库
-	db, err := sql.Open("mysql", "root:ly15984093508@tcp(website-mysql:3306)/blog")
+	r := router.Router()
+	gin.DefaultWriter = os.Stdout
+	err := r.Run(":8081")
 	if err != nil {
-		fmt.Println("Error connecting to the database:", err)
 		return
 	}
-	defer db.Close()
-
-	// 测试数据库连接
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Error pinging the database:", err)
-		return
-	}
-
-	fmt.Println("Successfully connected to MySQL!")
-
-	r := gin.Default() // 初始化gin实例
-	r.Run(":8081")
 }
